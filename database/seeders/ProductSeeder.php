74d7cf29fd61b501bac0a766a509f2de292fd276
mo121213132131 <hamoudishaaban16@gmail.com>
2025-06-27 14:14:35 +0200

    product  seeder update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,14 +12,21 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         $faker = Faker::create();
+        $allergies = ['gluten', 'pinda\'s', 'lactose', 'schaaldieren', 'noten', 'geen'];
 
         foreach (ProductCategory::all() as $category) {
-            for ($i = 0; $i < 3; $i++) {
+            for ($i = 0; $i < 5; $i++) {
                 Product::create([
                     'category_id' => $category->id,
-                    'name' => $faker->unique()->words(2, true),
+                    'name' => ucfirst($faker->unique()->words(2, true)),
                     'ean' => $faker->unique()->ean13(),
-                    'stock_quantity' => $faker->numberBetween(10, 100),
+                    'stock_quantity' => $faker->numberBetween(5, 200),
+                    'houdbaarheiddatum' => $faker->optional()->dateTimeBetween('now', '+6 months'),
+                    'omschrijving' => $faker->sentence(6),
+                    'status' => $faker->randomElement(['actief', 'inactief']),
+                    'isactief' => $faker->boolean(80),
+                    'opmerking' => $faker->optional(0.4)->sentence(),
+                    'soortalergie' => $faker->randomElement($allergies),
                 ]);
             }
         }
